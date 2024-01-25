4a453acb6f36abdd2b33a32da6da46e1eb697f02
Nishtha1209 <nishtha.kaigaonkar12@gmail.com>
2024-01-25 11:19:33 +0530

    ticketClassmade

---
+++ Passenger.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "ticket.h"
+
+using namespace std;
+
+class Passenger{
+    private: 
+            string name;
+            // string id;
+            string departure_city;
+            string destination;
+            
+            //
+};
