0090cbadecf1956e7f1a753d33739be2e033d526
meamitverma <amit.verma.22080@gmail.com>
2024-01-25 12:24:17 +0530

    passenger modified

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 int main() {
     // welcome to the sealink
+    cout << "Welcome to SeaLink Travels" << endl;
     // find the desired cruise for the user where to where
     cout << "Enter origin:" << endl;
+    string origin_city, dest_city;
+    
     Passenger passenger;
+    passenger.
 
     // show the available cruise
     // take the passenger details for proceeding to the seat booking
