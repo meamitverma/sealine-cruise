4669f10477e16d0833ba0fc3b851c6343f7dbb78
meamitverma <amit.verma.22080@gmail.com>
2024-01-25 12:03:55 +0530

    add data members to passengers

---
+++ Passenger.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "ticket.h"
+
+
+using namespace std;
+
+class Passenger{
+private: 
+    string name;
+    int age;
+    string departure_city;
+    string arrival_city;
+    string seat_type; // business or economy
+
+public:
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include<bits/stdc++.h>
 #include "Cruise.h"
+#include "Passenger.h"
+
 using namespace std;
 
 int main() {
-    
+    // welcome to the sealink
+    // find the desired cruise for the user where to where
+    cout << "Enter origin:" << endl;
+    Passenger passenger;
+
+    // show the available cruise
+    // take the passenger details for proceeding to the seat booking
 
     return 0;
 }
